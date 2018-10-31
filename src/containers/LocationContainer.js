2c30f59bdd7d0646c0a2f4c316d26ce3f66472f6
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-31 11:15:38 +0000

    Make request for countries api

--- src/containers/LocationContainer.js
+++ src/containers/LocationContainer.js
@@ -6,12 +6,17 @@ export default class LocationContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            imageUrl: null
+            imageUrl: null,
+            countries: []
         }
     }
 
 
     componentDidMount() {
+        fetch('https://restcountries.eu/rest/v2/all')
+            .then(response => response.json())
+            .then(countries => this.setState({countries: countries}) )
+
         fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=-3.18&lat=55.95&dim=0.122&cloud_score=True&api_key=pRV8OiANGrU9cEF806ZYsE9FdesQCvGBGoB6Iq9m')
             .then(response => response.json())
             .then(imageInfo => this.setState({imageUrl: imageInfo.url}))

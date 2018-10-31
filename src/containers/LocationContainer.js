56587e77c8baa3361e4fe15715e0a17044e46335
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-31 10:58:03 +0000

    Request made at the location container

--- src/containers/LocationContainer.js
+++ src/containers/LocationContainer.js
@@ -2,6 +2,20 @@ import React, {Component} from 'react';
 
 export default class LocationContainer extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageUrl: null
+        }
+    }
+
+
+    componentDidMount() {
+        fetch('https://api.nasa.gov/planetary/earth/imagery/?lon=-3.18&lat=55.95&dim=0.122&cloud_score=True&api_key=pRV8OiANGrU9cEF806ZYsE9FdesQCvGBGoB6Iq9m')
+            .then(response => response.json())
+            .then(imageInfo => this.setState({imageUrl: imageInfo.url}))
+    }
+
     render() {
         return (
             <h2>Container working!</h2>
